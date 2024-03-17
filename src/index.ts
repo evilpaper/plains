fa998f7a51b313f6556c48204197ad34e59b80f8
Pelle Lundgren <pelle.lundgren@lysa.se>
2024-03-17 15:08:22 +0100

    Setup project

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript + Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Custom 404 page
+app.use((req: Request, res: Response) => {
+  res.type("text/plain");
+  res.status(404);
+  res.send("404 · Not found");
+});
+
+// Custom 500 page
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.message);
+  res.type("text/plain");
+  res.status(500);
+  res.send("500 · Server Error");
+});
