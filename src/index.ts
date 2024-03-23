5e5046bc1cc6da6b6ff166c6af024f4091d1d5a9
Pelle Lundgren <pellburt@gmail.com>
2024-03-23 19:04:49 +0100

    Decouple route handlers for testability

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,22 @@
 import express, { Request, Response, NextFunction } from "express";
 import { Eta } from "eta";
 import path from "path";
-import { getFortune } from "./lib/fortune";
+
+import { home, about, notFound, serverError } from "./lib/handlers";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-let viewpath = path.join(__dirname, "views");
-let eta = new Eta({ views: viewpath, cache: true });
+const viewpath = path.join(__dirname, "views");
+export let eta = new Eta({ views: viewpath, cache: true });
 
 app.use(express.static("public"));
 
-app.get("/about", (req: Request, res: Response) => {
-  res.send(
-    eta.render("about", {
-      title: "Plains",
-      name: "About",
-      fortune: `${getFortune()}`,
-    })
-  );
-});
+app.get("/about", about);
+app.get("/", home);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(eta.render("home", { title: "Plains", name: "Plains" }));
-});
-
-// Custom 404 page
-app.use((req: Request, res: Response) => {
-  res.status(404);
-  res.send(eta.render("404", { title: "404" }));
-});
-
-// Custom 500 page
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.message);
-  res.status(500);
-  res.send(eta.render("500", { title: "500" }));
-});
+app.use(notFound); // Custom 404 page
+app.use(serverError); // Custom 500 page
 
 app.listen(port, () => {
   console.log(
---
+++ src/lib/handlers.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { eta } from "../index";
+import { getFortune } from "./fortune";
+
+export const home = (req: Request, res: Response) => {
+  res.send(eta.render("home", { title: "Plains", name: "Plains" }));
+};
+
+export const about = (req: Request, res: Response) => {
+  res.send(
+    eta.render("about", {
+      title: "Plains",
+      name: "About",
+      fortune: `${getFortune()}`,
+    })
+  );
+};
+
+export const notFound = (req: Request, res: Response) => {
+  res.status(404);
+  res.send(eta.render("404", { title: "404" }));
+};
+
+export const serverError = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err.message);
+  res.status(500);
+  res.send(eta.render("500", { title: "500" }));
+};
