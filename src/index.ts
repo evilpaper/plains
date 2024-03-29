7bfba78891bce9665fd4e5e069659dd4469263d0
Pelle Lundgren <pellburt@gmail.com>
2024-03-29 14:08:06 +0100

    Add route for request headers

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,13 @@ export let eta = new Eta({ views: viewpath, cache: true });
 
 app.use(express.static("public"));
 
+app.get("/headers", (req, res) => {
+  res.type("text/plain");
+  const headers = Object.entries(req.headers).map(
+    ([key, value]) => `${key}: ${value}`
+  );
+  res.send(headers.join("\n"));
+});
 app.get("/about", about);
 app.get("/", home);
 
